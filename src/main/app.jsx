df4e03aa497eb224525b89418493d53224ee17f9
Roger-Franco <rogerfrancogalt@gmail.com>
2022-02-24 12:12:15 -0400

    Componente Cabeçalho

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,9 +1,11 @@
 import '../common/template/dependencies'
 import React from "react";
 
+import Header from '../common/template/header'
+
 export default props => (
   <div className="wrapper">
     <h1>App1</h1>
-
+    <Header />
   </div>
 );
