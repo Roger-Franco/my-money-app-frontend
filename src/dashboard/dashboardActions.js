e04d79b69af6ad923d19e2a7724310d99dcda58c
Roger-Franco <rogerfrancogalt@gmail.com>
2022-02-27 16:20:09 -0400

    Integrando Dashboard com redux #02

---
+++ src/dashboard/dashboardActions.js
@@ -0,0 +1,10 @@
+import { BILLING_SUMMARY_FETCHED } from './actionTypes'
+import axios from 'axios'
+const BASE_URL = 'http://localhost:3003/api'
+export function getSummary() {
+  const request = axios.get(`${BASE_URL}/billingCycles/summary`)
+  return {
+    type: 'BILLING_SUMMARY_FETCHED',
+    payload: request
+  }
+}
