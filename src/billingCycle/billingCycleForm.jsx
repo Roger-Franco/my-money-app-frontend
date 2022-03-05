060a525f66dc3ee094fd6f432e3ea79ea39234bd
Roger-Franco <rogerfrancogalt@gmail.com>
2022-03-05 16:48:24 -0400

    Componente CreditList Básico

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
-import { init } from './billingCycleActions'
 
+import { init } from './billingCycleActions'
 import labelAndInput from '../common/form/labelAndInput';
+import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
   render() {
@@ -20,9 +21,12 @@ class BillingCycleForm extends Component {
             label='Mês' cols='12 4' placeholder='Informe o mês' />
           <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
             label='Ano' cols='12 4' placeholder='Informe o ano' />
+          <CreditList cols='12 6' />
         </div>
         <div className="box-footer">
-          <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
+          <button type='submit' className={`btn btn-${this.props.submitClass}`}>
+            {this.props.submitLabel}
+          </button>
           <button type='button' className="btn btn-default"
             onClick={this.props.init}>Cancelar</button>
         </div>
