27bec4ee0a7cbd8c5e62f242c056875107089682
Roger-Franco <rogerfrancogalt@gmail.com>
2022-05-19 23:12:58 -0400

    Finalização

--- src/common/template/menuItem.jsx
+++ src/common/template/menuItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
   <li>
     <a href={props.path}>
-      <i className={`fa fa-${props.icon}`} href=""></i> {props.label}
+      <i className={`fa fa-${props.icon}`} href=""></i> <span>{props.label}</span>
     </a>
   </li>
 )
